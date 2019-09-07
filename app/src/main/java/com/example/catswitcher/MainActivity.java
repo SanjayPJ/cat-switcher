44b2d9da4d94f4f24e6e0d61b13980315385321a
sanjaypj <sanjaypjayan2000@gmail.com>
2019-09-07 14:09:33 -0400

    thoughts removed

---
+++ app/src/main/java/com/example/catswitcher/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.catswitcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void change_cat(View view){
+        ImageView image = (ImageView) findViewById(R.id.cat_image);
+        image.setImageResource(R.drawable.cat2);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+}
